0e95e23d927156da2ff45d1b7b5a9fa06112ff05
lucaspaiola <lucas.v.paiola@gmail.com>
2024-03-19 14:44:45 -0300

    Usando lowdash para repetição de testes

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,8 @@
-it('testa a página da política de privacidade de forma independente', function() {
-    cy.visit('./src/privacy.html')
-
-    cy.contains('Talking About Test').should('be.visible')
+// Utilizacao do lowdash para executar o mesmo teste 5 vezes, testando sua confiabilidade
+Cypress._.times(5, function() {
+    it('testa a página da política de privacidade de forma independente', function() {
+        cy.visit('./src/privacy.html')
+    
+        cy.contains('Talking About Test').should('be.visible')
+    })
 })
